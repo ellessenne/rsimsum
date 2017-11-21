f3cdcd52c02788bfb88d6a7e481a3507a8cff719
Alessandro Gasparini <ag475@le.ac.uk>
2017-11-21 15:06:29 +0000

    Documentation is now markdown-based

--- R/data.R
+++ R/data.R
@@ -1,16 +1,19 @@
 #' Example of a simulation study
 #'
-#' A dataset from a simulation study comparing different ways to handle missing covariates when fitting a Cox model (White and Royston, 2009). One thousand datasets were simulated, each containing normally distributed covariates \eqn{x} and \eqn{z} and time-to-event outcome. Both covariates has 20\% of their values deleted independently of all other variables so the data became missing completely at random (Little and Rubin, 2002). Each simulated dataset was analysed in three ways. A Cox model was fit to the complete cases (\code{CC}). Then two methods of multiple imputation using chained equations (van Buuren, Boshuizen, and Knook, 1999) were used. The \code{MI_LOGT} method multiply imputes the missing values of \eqn{x} and \eqn{z} with the outcome included as \eqn{\log (t)} and \eqn{d}, where \eqn{t} is the survival time and \eqn{d} is the event indicator. The \code{MI_T} method is the same except that \eqn{\log (t)} is replaced by \eqn{t} in the imputation model. The results are stored in long format.
+#' A dataset from a simulation study comparing different ways to handle missing covariates when fitting a Cox model (White and Royston, 2009). One thousand datasets were simulated, each containing normally distributed covariates \eqn{x} and \eqn{z} and time-to-event outcome. Both covariates has 20\% of their values deleted independently of all other variables so the data became missing completely at random (Little and Rubin, 2002). Each simulated dataset was analysed in three ways. A Cox model was fit to the complete cases (`CC`). Then two methods of multiple imputation using chained equations (van Buuren, Boshuizen, and Knook, 1999) were used. The `MI_LOGT` method multiply imputes the missing values of \eqn{x} and \eqn{z} with the outcome included as \eqn{\log (t)} and \eqn{d}, where \eqn{t} is the survival time and \eqn{d} is the event indicator. The \code{MI_T} method is the same except that \eqn{\log (t)} is replaced by \eqn{t} in the imputation model. The results are stored in long format.
 #'
 #' @format A data frame with 3,000 rows and 4 variables:
-#' \describe{
-#'   \item{dataset}{simulated dataset number}
-#'   \item{method}{method used (\code{CC}, \code{MI_LOGT} or \code{MI_T})}
-#'   \item{b}{point estimate}
-#'   \item{se}{standard error of the point estimate}
-#' }
-#' @references White, I.R., and P. Royston. 2009. Imputing missing covariate values for the Cox model. Statistics in Medicine 28: 1982-1998 \url{http://dx.doi.org/10.1002/sim.3618}
-#' @references Little, R.J.A., and D.B. Rubin. 2002. Statistical analysis with missing data. 2nd ed. Hoboken, NJ: Wiley \url{http://dx.doi.org/10.1002/9781119013563}
-#' @references van Buuren, S., H.C. Boshuizen, and D.L. Knook. 1999. Multiple imputation of missing blood pressure covariates in survival analysis. Statistics in Medicine 18: 681-694 \url{http://dx.doi.org/10.1002/(SICI)1097-0258(19990330)18:6<681::AID-SIM71>3.0.CO;2-R}
+#' * `dataset` Simulated dataset number.
+#' * `method` Method used (`CC`, `MI_LOGT` or `MI_T`).
+#' * `b` Point estimate.
+#' * `se` Standard error of the point estimate.
+#'
+#' @examples
+#' library(rsimsum)
+#' data("MIsim")
+#'
+#' @references White, I.R., and P. Royston. 2009. Imputing missing covariate values for the Cox model. Statistics in Medicine 28: 1982-1998 [https://doi.org/10.1002/sim.3618](https://doi.org/10.1002/sim.3618)
+#' @references Little, R.J.A., and D.B. Rubin. 2002. Statistical analysis with missing data. 2nd ed. Hoboken, NJ: Wiley [https://doi.org/10.1002/9781119013563](https://doi.org/10.1002/9781119013563)
+#' @references van Buuren, S., H.C. Boshuizen, and D.L. Knook. 1999. Multiple imputation of missing blood pressure covariates in survival analysis. Statistics in Medicine 18: 681-694 [https://doi.org/10.1002/(SICI)1097-0258(19990330)18:6<681::AID-SIM71>3.0.CO;2-R]([https://doi.org/10.1002/(SICI)1097-0258(19990330)18:6<681::AID-SIM71>3.0.CO;2-R)
 
 "MIsim"
--- R/nsim.R
+++ R/nsim.R
@@ -1,18 +1,18 @@
 #' Compute number of simulations required
 #'
-#' The function \code{nsim} computes the number of simulations \eqn{B}{B} to perform based on the accuracy of an estimate of interest, using the following equation:
+#' The function `nsim` computes the number of simulations \eqn{B}{B} to perform based on the accuracy of an estimate of interest, using the following equation:
 #' \deqn{B = \left( \frac{(Z_{1 - \alpha / 2} + Z_{1 - theta}) \sigma}{\delta} \right) ^ 2,}{B = [((Z(1 - \alpha / 2) + Z(1 - \theta)) \sigma) / \delta] ^ 2 }
 #' where \eqn{\delta} is the specified level of accuracy of the estimate of interest you are willing to accept (i.e. the permissible difference from the true value \eqn{\beta}{\beta}), \eqn{Z_{1 - \alpha / 2}}{Z(1 - \alpha / 2)} is the \eqn{(1 - \alpha / 2)}{(1 - \alpha / 2)} quantile of the standard normal distribution, \eqn{Z_{1 - \theta}}{Z(1 - \theta)} is the \eqn{(1 - \theta)}{(1 - \theta)} quantile of the standard normal distribution with \eqn{(1 - \theta)}{1 - \theta} being the power to detect a specific difference from the true value as significant, and \eqn{\sigma ^ 2}{\sigma ^ 2]} is the variance of the parameter of interest.
 #'
-#' @param alpha Significance level. Must be a value between 0 and 1
-#' @param sigma Variance for the parameter of interest
-#' @param delta Specified level of accuracy of the estimate of interest you are willing to accept
-#' @param power Power to detect a specific difference from the true value as significant. Must be a value between 0 and 1. Defaults to 0.5, e.g. a power of 50\%
+#' @param alpha Significance level. Must be a value between 0 and 1.
+#' @param sigma Variance for the parameter of interest.
+#' @param delta Specified level of accuracy of the estimate of interest you are willing to accept.
+#' @param power Power to detect a specific difference from the true value as significant. Must be a value between 0 and 1. Defaults to 0.5, e.g. a power of 50\%.
 #'
 #' @return A scalar value \eqn{B}{B} representing the number of simulations to perform based on the accuracy required.
 #' @export
 #'
-#' @references Burton, A., Douglas G. Altman, P. Royston. et al. 2006. The design of simulation studies in medical statistics. Statistics in Medicine 25: 4279-4292 \url{http://dx.doi.org/10.1002/sim.2673}
+#' @references Burton, A., Douglas G. Altman, P. Royston. et al. 2006. The design of simulation studies in medical statistics. Statistics in Medicine 25: 4279-4292 [https://doi.org/10.1002/sim.2673](https://doi.org/10.1002/sim.2673)
 #'
 #' @examples
 #' # Number of simulations required to produce an estimate to within 5%
