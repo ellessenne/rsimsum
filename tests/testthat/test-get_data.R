453ac6aca725565b9024851dd28f03da0418b287
Alessandro Gasparini <ag475@le.ac.uk>
2018-01-22 16:42:17 +0000

    Added a couple of tests for new sstat argument of get_data

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -6,9 +6,22 @@ test_that("get_data.simsum returns a data.frame", {
   expect_s3_class(object = get_data(x), class = "data.frame")
 })
 
+test_that("get_data.simsum asking for bias returns only bias", {
+  data(MIsim)
+  x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
+  expect_true(object = all(get_data(x, sstat = "bias")$stat == "bias"))
+})
+
 test_that("get_data.summary.simsum returns a data.frame", {
   data(MIsim)
   x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
   s <- summary(x)
   expect_s3_class(object = get_data(s), class = "data.frame")
+})
+
+test_that("get_data.summary.simsum asking for bias returns only bias", {
+  data(MIsim)
+  x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
+  s <- summary(x)
+  expect_true(object = all(get_data(x, sstat = "bias")$stat == "bias"))
 })
