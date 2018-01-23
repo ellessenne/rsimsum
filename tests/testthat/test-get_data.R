ff7406abb37771f081e67b008f78c61476a3e0bc
Alessandro Gasparini <ag475@le.ac.uk>
2018-01-23 12:33:11 +0000

    Added a couple of tests more for get_data

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -6,6 +6,14 @@ test_that("get_data.simsum returns a data.frame", {
   expect_s3_class(object = get_data(x), class = "data.frame")
 })
 
+test_that("get_data.simsum asking for a subset of summary statistics only", {
+  data(MIsim)
+  x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
+  gd <- get_data(x, sstat = c("nsim", "bias", "cover"))
+  gd
+  expect_equal(object = nrow(gd), expected = 3 * length(unique(MIsim$method)))
+})
+
 test_that("get_data.simsum asking for bias returns only bias", {
   data(MIsim)
   x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
@@ -17,6 +25,15 @@ test_that("get_data.summary.simsum returns a data.frame", {
   x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
   s <- summary(x)
   expect_s3_class(object = get_data(s), class = "data.frame")
+})
+
+test_that("get_data.summary.simsum asking for a subset of summary statistics only", {
+  data(MIsim)
+  x <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", mcse = TRUE)
+  s <- summary(x)
+  gd <- get_data(s, sstat = c("nsim", "bias", "cover"))
+  gd
+  expect_equal(object = nrow(gd), expected = 3 * length(unique(MIsim$method)))
 })
 
 test_that("get_data.summary.simsum asking for bias returns only bias", {
