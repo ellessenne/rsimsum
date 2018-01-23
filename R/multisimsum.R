47ff2aff7f14ad5112609056e83a7b2780a2921a
Alessandro Gasparini <ag475@le.ac.uk>
2018-01-23 10:40:08 +0000

    Housekeeping and started working on summary by estimands

---
+++ R/multisimsum.R
@@ -0,0 +1,19 @@
+multisimsum <- function(data,
+                        par,
+                        true,
+                        estvarname,
+                        se,
+                        methodvar = NULL,
+                        ref = NULL,
+                        df = NULL,
+                        dropbig = FALSE,
+                        max = 10,
+                        semax = 100,
+                        level = 0.95,
+                        by = NULL,
+                        mcse = TRUE,
+                        sanitise = TRUE,
+                        na.rm = TRUE,
+                        na.pair = TRUE) {
+
+}
