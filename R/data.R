1146526686a2efb217409bd749fa047ef794d787
Alessandro Gasparini <ag475@le.ac.uk>
2018-01-26 11:22:11 +0000

    Updated frailty dataset

--- R/data.R
+++ R/data.R
@@ -38,15 +38,14 @@
 #' frailty
 #'
 #' @title Example of a simulation study on frailty survival models
-#' @description A dataset from a simulation study comparing frailty flexible parametric models fitted using penalised likelihood to semiparametric frailty models. Both models are fitted assuming a Gamma and a log-Normal frailty. One thousand datasets were simulated, each containing a binary treatment variable with a log-hazard ratio of -0.50. Clustered survival data was simulated assuming 50 clusters of 50 individuals each, with a mixture Weibull baseline hazard function and a true Gamma frailty. The comparison involves estimates of the log-treatment effect, and estimates of heterogeneity (i.e. the estimated frailty variance).
-#' @format A data frame with 24,000 rows and 7 variables:
+#' @description A dataset from a simulation study comparing frailty flexible parametric models fitted using penalised likelihood to semiparametric frailty models. Both models are fitted assuming a Gamma and a log-Normal frailty. One thousand datasets were simulated, each containing a binary treatment variable with a log-hazard ratio of -0.50. Clustered survival data was simulated assuming 50 clusters of 50 individuals each, with a mixture Weibull baseline hazard function and a frailty following either a Gamma or a Log-Normal distribution. The comparison involves estimates of the log-treatment effect, and estimates of heterogeneity (i.e. the estimated frailty variance).
+#' @format A data frame with 16,000 rows and 6 variables:
 #' * `i` Simulated dataset number.
 #' * `b` Point estimate.
 #' * `se` Standard error of the point estimate.
 #' * `par` The estimand. `trt` is the log-treatment effect, `fv` is the variance of the frailty.
+#' * `fv_dist` The true frailty distribution.
 #' * `model` Method used (`Cox, Gamma`, `Cox, Log-Normal`, `RP(P), Gamma`, or `RP(P), Log-Normal`).
-#' * `fv` The true frailty variance.
-#' * `true` The true value of the estimands.
 #'
 #' @examples
 #' data("frailty", package = "rsimsum")
