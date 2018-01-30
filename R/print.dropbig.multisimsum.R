037b41e965d79c918e1104e92b3c1351aa1217e4
Alessandro Gasparini <ag475@le.ac.uk>
2018-01-30 13:27:23 +0000

    Fixed a problem when printing 'dropbig' objects

--- R/print.dropbig.multisimsum.R
+++ R/print.dropbig.multisimsum.R
@@ -10,14 +10,12 @@ print.dropbig.multisimsum <- function(x, ...) {
   if (!is.null(x)) {
     if (nrow(x$big_estvarname) > 0) {
       cat("Dropped point estimates:\n")
-      names(x$big_estvarname) <- c("Row number", "Point estimate")
       print(x$big_estvarname, row.names = FALSE)
     } else {
       cat("No point estimates were dropped, all were within the limits defined by `max`\n")
     }
 
     if (nrow(x$big_se) > 0) {
-      cat("\nDropped standard errors:\n")
       names(x$big_se) <- c("Row number", "SE")
       print(x$big_se, row.names = FALSE)
     } else {
--- R/print.dropbig.simsum.R
+++ R/print.dropbig.simsum.R
@@ -10,7 +10,6 @@ print.dropbig.simsum <- function(x, ...) {
   if (!is.null(x)) {
     if (nrow(x$big_estvarname) > 0) {
       cat("Dropped point estimates:\n")
-      names(x$big_estvarname) <- c("Row number", "Point estimate")
       print(x$big_estvarname, row.names = FALSE)
     } else {
       cat("No point estimates were dropped, all were within the limits defined by `max`\n")
@@ -18,7 +17,6 @@ print.dropbig.simsum <- function(x, ...) {
 
     if (nrow(x$big_se) > 0) {
       cat("\nDropped standard errors:\n")
-      names(x$big_se) <- c("Row number", "SE")
       print(x$big_se, row.names = FALSE)
     } else {
       cat("\nNo standard errors estimates were dropped, all were within the limits defined by `semax`\n")
