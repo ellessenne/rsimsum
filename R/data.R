e6bcf69a131b42cba8d32ffcfe78cfabb9e6e50c
Alessandro Gasparini <lssndrgsprn@gmail.com>
2018-01-31 20:49:10 +0000

    Fixes for CRAN submission

--- R/data.R
+++ R/data.R
@@ -13,7 +13,6 @@
 #'
 #' @references White, I.R., and P. Royston. 2009. Imputing missing covariate values for the Cox model. Statistics in Medicine 28(15):1982-1998 \doi{10.1002/sim.3618}
 #' @references Little, R.J.A., and D.B. Rubin. 2002. Statistical analysis with missing data. 2nd ed. Hoboken, NJ: Wiley \doi{10.1002/9781119013563}
-#' @references van Buuren, S., H.C. Boshuizen, and D.L. Knook. 1999. Multiple imputation of missing blood pressure covariates in survival analysis. Statistics in Medicine 18(6):681-694 \doi{10.1002/(SICI)1097-0258(19990330)18:6<681::AID-SIM71>3.0.CO;2-R}
 "MIsim"
 
 #' relhaz
