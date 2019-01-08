46ff9fe3356a6ed40e045ec7b70f19d9b2cc5cc8
Alessandro Gasparini <ellessenne@gmail.com>
2019-01-08 11:44:25 +0200

    Style

--- data-raw/internal-data-testing.R
+++ data-raw/internal-data-testing.R
@@ -5,14 +5,14 @@ library(usethis)
 data("MIsim", package = "rsimsum")
 haven::write_dta(data = MIsim, path = "data-raw/MIsim.dta")
 data("relhaz", package = "rsimsum")
-relhaz$model = as.numeric(factor(relhaz$model))
+relhaz$model <- as.numeric(factor(relhaz$model))
 haven::write_dta(data = relhaz, path = "data-raw/relhaz.dta")
 
 # Requires Stata-MP with terminal utilities installed
 system("stata-mp -b do data-raw/internal-data-testing.do")
 
-MIsim_res_stata = haven::read_dta(file = "data-raw/MIsim_res_stata.dta")
-relhaz_res_stata = haven::read_dta(file = "data-raw/relhaz_res_stata.dta")
+MIsim_res_stata <- haven::read_dta(file = "data-raw/MIsim_res_stata.dta")
+relhaz_res_stata <- haven::read_dta(file = "data-raw/relhaz_res_stata.dta")
 
 usethis::use_data(MIsim_res_stata, relhaz_res_stata, internal = TRUE, overwrite = TRUE)
 
