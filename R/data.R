fe2e24de1f45a9f326c3da50ac23b5bfbb7bb4b7
Alessandro Gasparini <ellessenne@gmail.com>
2019-02-21 11:51:36 +0000

    Updates following notes on win-builder

--- R/data.R
+++ R/data.R
@@ -24,7 +24,7 @@
 #'
 #' @examples
 #' data("relhaz", package = "rsimsum")
-#' @references Cox D.R. 1972. Regression models and life-tables. Journal of the Royal Statistical Society, Series B (Methodological) 34(2):187-220. \url{http://www.jstor.org/stable/2985181}
+#' @references Cox D.R. 1972. Regression models and life-tables. Journal of the Royal Statistical Society, Series B (Methodological) 34(2):187-220. \doi{10.1007/978-1-4612-4380-9_37}
 #' @references Royston, P. and Parmar, M.K. 2002. Flexible parametric proportional-hazards and proportional-odds models for censored survival data, with application to prognostic modelling and estimation of treatment effects. Statistics in Medicine 21(15):2175-2197 \doi{10.1002/sim.1203}
 "relhaz"
 
