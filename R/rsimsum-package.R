d359c89039fc93e8bd7ef4fd21c8d77f568d1bf7
Alessandro Gasparini <ellessenne@gmail.com>
2019-11-14 20:00:57 +0100

    Added global variable required for density and hexbin plots

--- R/rsimsum-package.R
+++ R/rsimsum-package.R
@@ -12,4 +12,4 @@
 NULL
 
 # Quiets concerns of R CMD check re: variable names used internally
-if (getRversion() >= "2.15.1") utils::globalVariables(c(".scenario", ".dgm", "X", "Y", "contrast", "cover_lower", "cover_upper", "covering", "est", "lower", "upper"))
+if (getRversion() >= "2.15.1") utils::globalVariables(c(".scenario", ".dgm", "X", "Y", "contrast", "cover_lower", "cover_upper", "covering", "est", "lower", "upper", "level"))
