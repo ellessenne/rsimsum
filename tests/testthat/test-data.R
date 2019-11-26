e98fcf1477fe844333fd21c7cb2ccae66ce6f0bd
Alessandro Gasparini <ellessenne@gmail.com>
2019-11-26 09:47:04 +0100

    Documented t-test data

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -14,3 +14,14 @@ test_that("loading frailty works", {
   data("frailty", package = "rsimsum")
   expect_s3_class(object = frailty, class = "data.frame")
 })
+
+test_that("loading nlp works", {
+  data("nlp", package = "rsimsum")
+  expect_s3_class(object = nlp, class = "data.frame")
+})
+
+test_that("loading tt works", {
+  data("tt", package = "rsimsum")
+  expect_s3_class(object = tt, class = "data.frame")
+  expect_equal(object = dim(tt), expected = c(800, 8))
+})
