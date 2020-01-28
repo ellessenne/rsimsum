92fa9bd0e8804c30ab79f4222dab65696268f9a6
Alessandro Gasparini <alessandro.gasparini@ki.se>
2020-01-28 10:38:34 +0100

    Added more tests for #15

--- tests/testthat/test-#15.R
+++ tests/testthat/test-#15.R
@@ -12,6 +12,10 @@ testthat::test_that("simsum with 'true' as a column returns the same results as 
   s1 <- simsum(data = tt, estvarname = "diff", true = "true", se = "se", methodvar = "method", by = "dgm")
   s2 <- simsum(data = tt, estvarname = "diff", true = -1, se = "se", methodvar = "method", by = "dgm")
   testthat::expect_identical(object = get_data(s1), expected = get_data(s2))
+
+  s1 <- simsum(data = tt, estvarname = "diff", true = "true", se = "se", ci.limits = c(-1.5, -0.5), methodvar = "method", by = "dgm")
+  s2 <- simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c(-1.5, -0.5), methodvar = "method", by = "dgm")
+  testthat::expect_identical(object = get_data(s1), expected = get_data(s2))
 })
 
 testthat::test_that("simsum with 'true' as a column returns different results compared to using a scalar value if each row has a different value", {
@@ -24,5 +28,9 @@ testthat::test_that("simsum with 'true' as a column returns different results co
 
   s1 <- simsum(data = tt, estvarname = "diff", true = "true", se = "se", methodvar = "method", by = "dgm")
   s2 <- simsum(data = tt, estvarname = "diff", true = -1, se = "se", methodvar = "method", by = "dgm")
+  testthat::expect_false(object = identical(get_data(s1), get_data(s2)))
+
+  s1 <- simsum(data = tt, estvarname = "diff", true = "true", se = "se", ci.limits = c(-1.5, -0.5), methodvar = "method", by = "dgm")
+  s2 <- simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c(-1.5, -0.5), methodvar = "method", by = "dgm")
   testthat::expect_false(object = identical(get_data(s1), get_data(s2)))
 })
