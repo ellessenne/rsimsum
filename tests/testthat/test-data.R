c7bcc662ebdee1d8f7de5e5643bfb6fc05d7bab5
Alessandro Gasparini <alessandro.gasparini@ki.se>
2020-01-28 11:38:56 +0100

    Closes #20

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,27 +1,27 @@
-context("data")
+testthat::context("data")
 
-test_that("loading MIsim works", {
+testthat::test_that("loading MIsim works", {
   data("MIsim", package = "rsimsum")
-  expect_s3_class(object = MIsim, class = "data.frame")
+  testthat::expect_s3_class(object = MIsim, class = "data.frame")
 })
 
-test_that("loading relhaz works", {
+testthat::test_that("loading relhaz works", {
   data("relhaz", package = "rsimsum")
-  expect_s3_class(object = relhaz, class = "data.frame")
+  testthat::expect_s3_class(object = relhaz, class = "data.frame")
 })
 
-test_that("loading frailty works", {
+testthat::test_that("loading frailty works", {
   data("frailty", package = "rsimsum")
-  expect_s3_class(object = frailty, class = "data.frame")
+  testthat::expect_s3_class(object = frailty, class = "data.frame")
 })
 
-test_that("loading nlp works", {
+testthat::test_that("loading nlp works", {
   data("nlp", package = "rsimsum")
-  expect_s3_class(object = nlp, class = "data.frame")
+  testthat::expect_s3_class(object = nlp, class = "data.frame")
 })
 
-test_that("loading tt works", {
+testthat::test_that("loading tt works", {
   data("tt", package = "rsimsum")
-  expect_s3_class(object = tt, class = "data.frame")
-  expect_equal(object = dim(tt), expected = c(4000, 8))
+  testthat::expect_s3_class(object = tt, class = "data.frame")
+  testthat::expect_equal(object = dim(tt), expected = c(4000, 8))
 })
