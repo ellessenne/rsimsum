0bc9437e12edca3fd44a8dd201791a55be0bf9c6
Alessandro Gasparini <ellessenne@gmail.com>
2021-02-01 16:03:09 +0100

    Added reprex for #30

--- testing.R
+++ testing.R
@@ -1,5 +1,27 @@
 devtools::load_all()
 
+# Load library and data
+library(rsimsum)
+data("frailty", package = "rsimsum")
+
+# Duplicate last row and and to data
+frailty <- rbind.data.frame(frailty, frailty[(nrow(frailty) - 3):nrow(frailty), ])
+
+# Check nsim
+with(frailty, table(model, interaction(fv_dist, par)))
+
+ms1 <- multisimsum(
+  data = frailty,
+  par = "par",
+  true = c(trt = -0.50, fv = 0.75),
+  estvarname = "b",
+  se = "se",
+  ref = "Cox, Gamma",
+  methodvar = "model",
+  by = "fv_dist"
+)
+ms1
+
 ### All-in-one testing
 devtools::document()
 devtools::build_vignettes()
