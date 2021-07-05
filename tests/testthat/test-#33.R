c774a06cf7cc922d515f6e56105c1f35635bb228
Alessandro Gasparini <ellessenne@gmail.com>
2021-07-05 10:51:32 +0200

    Further updates tests for #33

--- tests/testthat/test-#33.R
+++ tests/testthat/test-#33.R
@@ -10,4 +10,7 @@ testthat::test_that("Test that power_df is actually used", {
   s3 <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method")
   testthat::expect_false(object = identical(x = tidy(s3)$est[tidy(s3)$stat == "power"], y = tidy(s2)$est[tidy(s2)$stat == "power"]))
   testthat::expect_identical(object = tidy(s1)$est[tidy(s1)$stat == "power"], expected = tidy(s3)$est[tidy(s3)$stat == "power"])
+
+  s4 <- simsum(data = MIsim, estvarname = "b", true = 0.5, se = "se", methodvar = "method", control = list(power_df = 1000))
+  testthat::expect_identical(object = tidy(s1)$est[tidy(s1)$stat == "power"], expected = tidy(s4)$est[tidy(s4)$stat == "power"])
 })
