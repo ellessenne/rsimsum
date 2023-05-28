00d14d8a430967c3a04ff83f48c1f85bf54d16bb
Alessandro Gasparini <git@ellessenne.xyz>
2023-05-28 15:23:54 +0200

    Adjusted names in tests

--- tests/testthat/test-#26.R
+++ tests/testthat/test-#26.R
@@ -14,14 +14,14 @@ testthat::test_that("expect equivalent values when passing lower, upper (based o
   data("tt", package = "rsimsum")
 
   s6 <- simsum(data = tt, estvarname = "diff", se = "se", df = "df", true = -1)
-  s7 <- simsum(data = tt, estvarname = "diff", se = "se", ci.limits = c("lower", "upper"), true = -1)
+  s7 <- simsum(data = tt, estvarname = "diff", se = "se", ci.limits = c("conf.low", "conf.high"), true = -1)
   testthat::expect_equal(object = tidy(s6), expected = tidy(s7))
 
   s6 <- simsum(data = tt, estvarname = "diff", se = "se", methodvar = "method", df = "df", true = -1)
-  s7 <- simsum(data = tt, estvarname = "diff", se = "se", methodvar = "method", ci.limits = c("lower", "upper"), true = -1)
+  s7 <- simsum(data = tt, estvarname = "diff", se = "se", methodvar = "method", ci.limits = c("conf.low", "conf.high"), true = -1)
   testthat::expect_equal(object = tidy(s6), expected = tidy(s7))
 
   s6 <- simsum(data = tt, estvarname = "diff", se = "se", methodvar = "method", by = "dgm", df = "df", true = -1)
-  s7 <- simsum(data = tt, estvarname = "diff", se = "se", methodvar = "method", by = "dgm", ci.limits = c("lower", "upper"), true = -1)
+  s7 <- simsum(data = tt, estvarname = "diff", se = "se", methodvar = "method", by = "dgm", ci.limits = c("conf.low", "conf.high"), true = -1)
   testthat::expect_equal(object = tidy(s6), expected = tidy(s7))
 })
--- tests/testthat/test-ci.limits.R
+++ tests/testthat/test-ci.limits.R
@@ -10,12 +10,12 @@ testthat::test_that("ci.limits breaks if logic vector", {
 })
 
 testthat::test_that("ci.limits works ok with string or numeric vectors", {
-  testthat::expect_s3_class(object = rsimsum::simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c("lower", "upper"), methodvar = "method", by = "dgm"), class = "simsum")
+  testthat::expect_s3_class(object = rsimsum::simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c("conf.low", "conf.high"), methodvar = "method", by = "dgm"), class = "simsum")
   testthat::expect_s3_class(object = rsimsum::simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c(-2, 0), methodvar = "method", by = "dgm"), class = "simsum")
 })
 
 testthat::test_that("ci.limits with string vector yields different values of coverage than the default", {
-  s <- rsimsum::simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c("lower", "upper"), methodvar = "method", by = "dgm")
+  s <- rsimsum::simsum(data = tt, estvarname = "diff", true = -1, se = "se", ci.limits = c("conf.low", "conf.high"), methodvar = "method", by = "dgm")
   s <- tidy(s, stats = "cover")
   sdef <- rsimsum::simsum(data = tt, estvarname = "diff", true = -1, se = "se", methodvar = "method", by = "dgm")
   sdef <- tidy(sdef, stats = "cover")
