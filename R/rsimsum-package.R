cfef860898537c071725fb198d28b9de48fb9491
Alessandro Gasparini <git@ellessenne.xyz>
2023-12-12 20:25:16 +0100

    *should* be okay?

--- R/rsimsum-package.R
+++ R/rsimsum-package.R
@@ -1,14 +1,9 @@
-#' @title Analysis of Simulation Studies Including Monte Carlo Error
-#'
-#' @description Summarise results from simulation studies and compute Monte Carlo
-#' standard errors of commonly used summary statistics.
-#' This package is modelled on the 'simsum' user-written command in 'Stata'
-#' (See White I.R., 2010 <https://www.stata-journal.com/article.html?article=st0200>).
-#'
-#' @name rsimsum
-#' @docType package
-#' @author Alessandro Gasparini (alessandro.gasparini@@ki.se)
 #' @import checkmate generics ggridges ggplot2 knitr lifecycle rlang scales stats
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
 
 # Quiets concerns of R CMD check re: variable names used internally
